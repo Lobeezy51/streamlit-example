e580235e9f32447e768ad6fe9186d37596dea213
Lobeezy51 <163198440+Lobeezy51@users.noreply.github.com>
2024-03-13 11:53:42 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,13 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Streamlit Example')
 st.write('Hello World! :sunglasses:')
 x = st.text_input('Favorte Movie')
 st.write(f'Your favorite movie is: {x}')
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40],
+}))
 
 is_clicked = st.button('Click Me')
