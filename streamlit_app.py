e4e3c3eb0dfecafe3949cec3f3f8362b7cd131e0
Lobeezy51 <163198440+Lobeezy51@users.noreply.github.com>
2024-03-13 11:55:48 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import altair as alt
 
 st.title('Streamlit Example')
 st.write('Hello World! :sunglasses:')
@@ -9,5 +11,12 @@ st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40],
 }))
+
+df = pd.DataFrame(
+    np.random.randn(200, 3),
+    columns=['a', 'b', 'c'])
+
+c = alt.Chart(df).mark_circle().encode(
+    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
 
 is_clicked = st.button('Click Me')
